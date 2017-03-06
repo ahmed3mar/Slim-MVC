1d7cf50d06f12c1d1f5d6a37c60675d616a978aa
Ahmed M. Ammar <ahmed3mar@outlook.com>
2017-03-06 13:47:33 +0200

    INIT

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -1,14 +1,4 @@
 <?php
 
-//Route::get('/name/{name}', function() {
-//
-//})->setName('ali');
-
 Route::get('/', 'IndexController@index');
 Route::get('/users', 'IndexController@users');
-
-//$app->get('/', 'App\Controllers\IndexController:index');
-
-//$app->get('/', function() {
-//    return view('home');
-//});
